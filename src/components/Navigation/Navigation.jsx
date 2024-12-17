d40e068b0c1bf9995356ed270a18ba6ea0a3bd83
alexz60 <alexqm@mail.ru>
2024-12-17 13:23:42 +0300

    release_v4_1

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -3,10 +3,8 @@ import s from './Navigation.module.css'
 import { UseDeviceDetect } from "../UseDeviceDetect/UseDeviceDetect"
 
 export const Navigation = () => {
-    const isMobile = UseDeviceDetect()
-
     return (
-        <nav className={isMobile ? [s.container, s.mobile].join(' ') : s.container}>
+        <nav className={UseDeviceDetect() ? [s.container, s.mobile].join(' ') : s.container}>
             <NavLink className={(navData) => navData.isActive ? [s.item, s.active].join(' ') : s.item} to="/generate" >Генерировать QR код</NavLink>
             <NavLink className={(navData) => navData.isActive ? [s.item, s.active].join(' ') : s.item} to="/scan" >Сканировать QR код</NavLink>
             <NavLink className={(navData) => navData.isActive ? [s.item, s.active].join(' ') : s.item} to="/generateHistory" >История генерирования</NavLink>
